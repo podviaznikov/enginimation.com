a5b4dd8ce91c17dc511acba722b21d898ef13bab
Anton Podviaznikov <podviaznikov@gmail.com>
2011-05-12 22:54:58 +0200

    added node.js server file

---
+++ server.js
@@ -0,0 +1,26 @@
+// enginimation.com
+// (c) 2011 Enginimation Studio (http://enginimation.com).
+// m.player may be freely distributed under the MIT license.
+// For all details and documentation:
+// https://github.com/podviaznikov/m.player
+var util = require('util'),
+    express = require('express'),
+    connect = require('connect'),
+    app = express.createServer();
+app.configure(function()
+{
+    //app.use(connect.favicon(__dirname + '/public/16.png'));
+    //logger
+    app.use(express.logger());
+    //router
+    app.use(app.router);
+    //public folder for static files
+    app.use(express.static(__dirname+'/public'));
+});
+app.get('/app.mf', function(req, res)
+{
+    res.header("Content-Type", "text/cache-manifest");
+    res.sendfile(__dirname + '/app.mf');
+});
+app.listen(8087);
+util.log('started app on 8087');
